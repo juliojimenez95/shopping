6cf21fa227476b5893f35647fd80397cdaa0837d
Julio Jimenez <JulioJimenegarcia@gmail.com>
2022-05-09 22:28:28 -0500

    block user

--- Shopping/Shopping/Controllers/AccountController.cs
+++ Shopping/Shopping/Controllers/AccountController.cs
@@ -49,7 +49,15 @@ namespace Shopping.Controllers
                     return RedirectToAction("Index", "Home");
                 }
 
-                ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
+                if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError(string.Empty, "Ha superado el máximo número de intentos, su cuenta está bloqueada, intente de nuevo en 5 minutos.");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
+                }
+
             }
 
             return View(model);
