7c638384ec3e6cafe76081d875441796d745bf13
Julio Jimenez <JulioJimenegarcia@gmail.com>
2022-05-30 14:39:56 -0500

    orden manager begin

--- Shopping/Shopping/Controllers/OrdersController.cs
+++ Shopping/Shopping/Controllers/OrdersController.cs
@@ -2,12 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Shopping.Data;
+using Shopping.Data.Entities;
 
 namespace Shopping.Controllers
 {
     [Authorize(Roles = "Admin")]
 
-    public class OrdersController
+    public class OrdersController : Controller
     {
         private readonly DataContext _context;
 
@@ -15,14 +16,36 @@ namespace Shopping.Controllers
         {
             _context = context;
         }
-        /*public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             return View(await _context.Sales
                 .Include(s => s.User)
                 .Include(s => s.SaleDetails)
                 .ThenInclude(sd => sd.Product)
                 .ToListAsync());
-        }*/
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Sale sale = await _context.Sales
+                .Include(s => s.User)
+                .Include(s => s.SaleDetails)
+                .ThenInclude(sd => sd.Product)
+                .ThenInclude(p => p.ProductImages)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sale);
+        }
+
 
     }
 }
